50b67d3d4d6b055c51a59f5b823fa20d1e249be3
robpwatkins <robpwatkins@gmail.com>
2022-07-10 16:37:42 -0500

    adding graphql

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`Server running on port ${port}!`));
